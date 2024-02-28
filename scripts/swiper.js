67bda41b75ed35b21a769d04c476a5de3d81fadd
Sam McEwan <samlmcewan@gmail.com>
2024-02-28 20:19:37 +0000

    hero slider, hero text size xl

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -24,3 +24,29 @@ const swiper = new Swiper('.swiper', {
   
    
   });
+
+  const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    // Autoplay
+  autoplay: {
+    delay: 5000, // Time in milliseconds between slides
+    disableOnInteraction: false, // Set to false if you want autoplay to continue even when user interacts with the swiper
+  },
+  
+  
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    //   hideOnClick: false 
+    // },
+  
+   
+  });
